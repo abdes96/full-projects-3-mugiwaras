6518534bb840686da89589ea9a14d58c02f1c4fd
Abdeslam Boutaarourt <abdeslam.boutaarourt@student.ehb.be>
2023-01-25 10:19:33 +0100

    firstpage

---
+++ script.js
@@ -0,0 +1,36 @@
+"use strict";
+window.onload = function () {
+  setup();
+};
+
+function setup() {
+  const image = document.querySelector(".placeholder img");
+  const text = document.querySelector(".info-text p");
+  let imageIndex = 0;
+  let clickCount = 0;
+  const texts = [
+    "Find your way through history ",
+    "Discover our exciting activites",
+    "Stay updated on our exhibitions",
+  ];
+  const images = ["06032016-DSC_5246.jpg", "museum2.jpg", "museum3.jpg"];
+  document.getElementById("buttonSkip").addEventListener("click", () => {
+    clickCount++;
+    //images//
+    imageIndex = (imageIndex + 1) % images.length;
+    image.src = "./images/" + images[imageIndex];
+
+    //circles//
+    let round = document.querySelector("#round" + (imageIndex + 1));
+    round.style.backgroundColor = "#30616B";
+    let prevRound = document.querySelector("#round" + imageIndex);
+    if (prevRound) prevRound.style.backgroundColor = "rgba(44,44,44,0.5)";
+    //text //
+    text.textContent = texts[imageIndex];
+
+    // next page
+    if (clickCount % 2 === 0) {
+      console.log("nextpage");
+    }
+  });
+}
